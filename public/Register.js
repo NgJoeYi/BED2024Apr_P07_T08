8cb15e6decfbfe586c8faaa86314333f69696c12
NgJoeYi <s10262850@connect.np.edu.sg>
2024-06-20 01:49:48 +0800

    when registered direct to login page

--- public/Register.js
+++ public/Register.js
@@ -48,6 +48,7 @@ document.getElementById('register-form').addEventListener('submit', async functi
 
         const result = await response.json();
         alert('Registration successful!');
+        window.location.href = 'Login.html';
     } catch (error) {
         // Display the error message
         alert(`Registration failed: ${error.message}`);
