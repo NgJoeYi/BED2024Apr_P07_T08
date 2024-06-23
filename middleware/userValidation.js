d69fe407f06852577646cfa9daaca131fb5ea7d5
NgJoeYi <s10262850@connect.np.edu.sg>
2024-06-24 04:02:50 +0800

    update middleware validateUser.js

--- middleware/userValidation.js
+++ middleware/userValidation.js
@@ -4,7 +4,7 @@ const Joi = require("joi");
 const validateUser = (req, res, next) => {
     const schema = Joi.object({
       name: Joi.string().min(3).max(50).required(),
-      dob: Joi.date().required(),
+      //dob: Joi.date().required(), i think dont need to valiate date here since html has validation for date and i am ok with how it looks like when it is shown
       email: Joi.string().email().max(100).required(),
       password: Joi.string().min(8).max(255).required(),
       role: Joi.string().valid('student', 'lecturer').required()
