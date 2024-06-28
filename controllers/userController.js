feaedc2d98d73b301dd663ecfa0b6f2bd385e1af
NgJoeYi <s10262850@connect.np.edu.sg>
2024-06-28 12:08:34 +0800

    update minor issue

--- controllers/userController.js
+++ controllers/userController.js
@@ -169,7 +169,7 @@ const getProfilePicByUserId = async (req, res) => {
             return res.status(404).send('User does not exist');
         }
 
-        const profilePic = await User.getProfilePicByUserId(userId);
+        let profilePic = await User.getProfilePicByUserId(userId);
         if (!profilePic) {
            profilePic = 'images/profilePic.jpeg'; // Default profile picture 
         }
