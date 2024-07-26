d6459630c902edcb90701a6d05a9fda9405031a2
NgJoeYi <s10262850@connect.np.edu.sg>
2024-07-27 04:16:25 +0800

    made quiz question actually correct

--- seed.js
+++ seed.js
@@ -357,31 +357,31 @@ async function run() {
         const insertQuestions = `
         INSERT INTO Questions (quiz_id, question_text, qnsImg, option_1, option_2, option_3, option_4, correct_option) VALUES
         -- Color Blindness Assessment Questions
-        (1, 'What number do you see in the image?', @colorBlindQns1, '8', '12', '5', 'None', '8'),
-        (1, 'What number do you see in the image?', @colorBlindQns2, '1', 'No', 'Not sure', 'I see nothing', '1'),
-        (1, 'What number do you see in the image?', @colorBlindQns3, '3', 'Blue and Yellow', 'Only Red', 'Only Green', '3'),
-        (1, 'What number do you see in the image?', @colorBlindQns4, '6', 'Squares', 'Triangles', 'None', '6'),
-        (1, 'Do you see a number in this image?', @colorBlindQns5, 'Yes, it is 5', 'Yes, it is 3', 'No', 'Not sure', 'Yes, it is 5'),
+        (1, 'What number do you see in the image?', @colorBlindQns1, '8', '12', '7', 'None', '7'),
+        (1, 'What number do you see in the image?', @colorBlindQns2, '1', '8', 'Not sure', 'I see nothing', '8'),
+        (1, 'What number do you see in the image?', @colorBlindQns3, '3', '5', '8', 'I do not see anything', '5'),
+        (1, 'What number do you see in the image?', @colorBlindQns4, '8', 'I do not see anything', '3', '6', '8'),
+        (1, 'Do you see a number in this image?', @colorBlindQns5, 'Yes, it is 5', 'Yes, it is 42', '41', 'Not sure', 'Yes, it is 42'),
 
         -- Visual Acuity Test Questions
-        (2, 'What is the smallest line you can read in this image?', @visualAcuityQns1, 'Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 3'),
-        (2, 'What letter do you see in the middle of this image?', @visualAcuityQns2, 'A', 'B', 'C', 'D', 'C'),
-        (2, 'Identify the direction of the "E" in this image.', @visualAcuityQns3, 'Up', 'Down', 'Left', 'Right', 'Right'),
-        (2, 'How many letters do you see in this image?', @visualAcuityQns4, '4', '5', '6', '7', '5'),
+        (2, 'What is the smallest line you can read in this image?', @visualAcuityQns1, 'Line 1: "T O Z"', 'Line 2: "L P E D"', 'Line 3: "E C F"', 'I am not sure', 'Line 3: "E C F"'),
+        (2, 'What letter do you see in the middle of this image?', @visualAcuityQns2, 'A', 'B', 'C', 'There are letters, but they are not in the middle', 'There are letters, but they are not in the middle'),
+        (2, 'Identify the direction of the "E" in this image.', @visualAcuityQns3, 'Up', 'Down', 'Left', 'There is no "E" in this image', 'There is no "E" in this image'),
+        (2, 'How many letters are there in the first row?', @visualAcuityQns4, '4', '2', '6', '7', '2'),
         (2, 'What is the color of the largest letter in this image?', @visualAcuityQns5, 'Red', 'Blue', 'Green', 'Black', 'Black'),
 
         -- Astigmatism Test Questions
         (3, 'Do the lines in this image appear to be straight and parallel?', @astigmatismQns1, 'Yes', 'No', 'Not sure', 'Somewhat', 'Yes'),
         (3, 'Do the circles in this image look equally clear?', @astigmatismQns2, 'Yes', 'No', 'Not sure', 'Somewhat', 'Yes'),
-        (3, 'What shape do you see in this image?', @astigmatismQns3, 'Circle', 'Oval', 'Square', 'Rectangle', 'Circle'),
+        (3, 'What shape do you see in this image?', @astigmatismQns3, 'Circle', 'Oval', 'Square', 'Rectangle', 'Square'),
         (3, 'Are there any blurry areas in this image?', @astigmatismQns4, 'Yes', 'No', 'Not sure', 'Somewhat', 'No'),
         (3, 'Do you see any wavy lines in this image?', @astigmatismQns5, 'Yes', 'No', 'Not sure', 'Somewhat', 'No'),
 
         -- Macular Degeneration Assessment Questions
-        (4, 'Do you see a grid of straight lines in this image?', @macularDegenerationQns1, 'Yes', 'No', 'Some lines are wavy', 'Some lines are missing', 'Yes'),
-        (4, 'Is there any distortion in the lines of this image?', @macularDegenerationQns2, 'Yes', 'No', 'Some lines are wavy', 'Some lines are missing', 'No'),
-        (4, 'Do you see any missing areas in this image?', @macularDegenerationQns3, 'Yes', 'No', 'Some areas are missing', 'Not sure', 'No'),
-        (4, 'What shapes do you see in this image?', @macularDegenerationQns4, 'Circles', 'Squares', 'Triangles', 'None', 'Squares'),
+        (4, 'Do you see a grid of straight lines in this image?', @macularDegenerationQns1, 'Yes', 'No', 'Some lines are wavy', 'Some lines are missing', 'Some lines are wavy'),
+        (4, 'Is there any distortion in the lines of this image?', @macularDegenerationQns2, 'Yes', 'No', 'Some lines are wavy', 'Some lines are missing', 'Yes'),
+        (4, 'Do you see any missing areas in this image?', @macularDegenerationQns3, 'Yes', 'No, but some lines are wavy', 'Some areas are missing', 'Not sure', 'No, but some lines are wavy'),
+        (4, 'What shapes do you see in this image?', @macularDegenerationQns4, 'Circles', 'Grids of wavy line', 'Triangles', 'None', 'Grids of wavy line'),
         (4, 'Is there any blur in the center of this image?', @macularDegenerationQns5, 'Yes', 'No', 'Somewhat', 'Not sure', 'No');
         `;
 
