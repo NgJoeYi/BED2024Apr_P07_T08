87d1f3a4bc4ee4fc5dedb1698933b32f3b92fe66
NgJoeYi <s10262850@connect.np.edu.sg>
2024-07-27 12:49:48 +0800

    added comment

--- middleware/quizzesMiddleware.js
+++ middleware/quizzesMiddleware.js
@@ -3,28 +3,37 @@ const Joi = require("joi");
 
 const validateCreateQuiz = (req, res, next) => {
   const schema = Joi.object({
-    title: Joi.string().required(),
-    description: Joi.string().allow(null, ''),
-    total_questions: Joi.number().integer().required(),
-    total_marks: Joi.number().integer().required(),
-    // created_by: Joi.number().integer().required(),
-    quizImg: Joi.string().base64().required(), // Require quizImg
-    });
+      title: Joi.string().required(),
+      description: Joi.string().allow(null, ''),
+      total_questions: Joi.number().integer().required(),
+      total_marks: Joi.number().integer().required(),
+      quizImg: Joi.string().base64().allow(null, ''), // Optional quizImg
+      questions: Joi.array().items(
+          Joi.object({
+              question_text: Joi.string().required(),
+              option_1: Joi.string().required(),
+              option_2: Joi.string().required(),
+              option_3: Joi.string().required(),
+              option_4: Joi.string().required(),
+              correct_option: Joi.number().integer().min(1).max(4).required(),
+              qnsImg: Joi.string().base64().allow(null, '') // Optional question image
+          })
+      ).min(1).required() // At least one question is required
+  });
 
-const validation = schema.validate(req.body, { abortEarly: false });
+  const validation = schema.validate(req.body, { abortEarly: false });
 
+  if (validation.error) {
+      const errors = validation.error.details.map((error) => error.message);
+      console.log('Validation errors:', errors); // Log the validation errors to the console
+      res.status(400).json({ message: "Validation error", errors });
+      return;
+  }
 
-if (validation.error) {
-    const errors = validation.error.details.map((error) => error.message);
-    console.log('Validation errors:', errors); // Log the validation errors to the console
-    res.status(400).json({ message: "Validation error", errors });
-    return;
-}
-
-next();
+  next();
 };
 
-// rmb check database schema to match!!!!!!!!!!!!!!!!
+// rmb check database schema to match!!!!!!!!!!!!!!!! using for create question in update question page
 const validateCreateQuestion = (req, res, next) => {
   const schema = Joi.object({
       quiz_id: Joi.number().integer().required(),
